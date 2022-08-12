11cb2728ba171261719903e4fb30a2538c746a62
Bob Fanger <b.fanger@wearetriple.com>
2022-08-12 11:16:04 +0200

    chore: Updated deps

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,28 +1,18 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
-const baseURL = "http://localhost:3000";
-const nodeVersion = parseInt(process.version.match(/v([0-9]+)\./)[1], 10);
-// https://github.com/nodejs/node/issues/40702
-const webServer =
-  nodeVersion >= 17
-    ? undefined
-    : {
-        command: "npm run dev",
-        url: baseURL,
-        reuseExistingServer: true,
-      };
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
   testDir: "./playwright/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   use: {
-    baseURL,
+    baseURL: "http://localhost:5173",
     trace: "retain-on-failure",
   },
-  webServer,
+  webServer: {
+    command: "npm run dev",
+    port: 5173,
+    reuseExistingServer: true,
+  },
 };
 
 export default config;
--- src/routes/api/react-version.json.ts
+++ src/routes/api/react-version.json.ts
@@ -2,7 +2,7 @@ import detectReactVersion from "$lib/internal/detectReactVersion";
 import type { RequestHandler } from "@sveltejs/kit";
 
 // eslint-disable-next-line import/prefer-default-export
-export const get: RequestHandler = async () => {
+export const GET: RequestHandler = async () => {
   return {
     body: await detectReactVersion(),
   };
