040216df2b1d09fb7bd2ec1ede91820f8e2ee542
Bob Fanger <bfanger@gmail.com>
2023-01-09 00:03:41 +0100

    chore: Upgraded deps + Moved from yarn to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import { sveltekit } from "@sveltejs/kit/vite";
+import react from "@vitejs/plugin-react";
 import { configDefaults, type UserConfig } from "vitest/config";
 
 const config: UserConfig = {
-  plugins: [sveltekit()],
+  plugins: [react({ fastRefresh: false }), sveltekit()],
   css: { devSourcemap: true },
   server: {
     fs: { allow: ["package"] },
