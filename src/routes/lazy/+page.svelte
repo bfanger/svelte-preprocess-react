7f6be0d1f91f41f630b0dc3d1d5873b98b68ead5
Bob Fanger <b.fanger@wearetriple.com>
2023-03-11 11:40:10 +0100

    chore: Updated deps

--- src/routes/lazy/+page.svelte
+++ src/routes/lazy/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import { browser } from "$app/environment";
   import { used } from "$lib";
-  import { lazy, Suspense } from "react";
+  import { lazy } from "react";
 
   const Counter = lazy(() => import("../../demo/react-components/Counter"));
-  used(Counter, Suspense);
+  used(Counter);
 </script>
 
 {#if browser}
