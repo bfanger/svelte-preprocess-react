4ca4d3fed752c9c791cba1e819744948030af67f
Bob Fanger <bfanger@gmail.com>
2024-10-20 02:19:15 +0200

    feat: Dynamic component example
    Fixes #11

---
+++ src/routes/dynamic/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Alert from "../../demo/react-components/Alert";
+  import Counter from "../../demo/react-components/Counter";
+  import { sveltify } from "svelte-preprocess-react";
+
+  let group = $state("alert");
+
+  let Component = $derived(sveltify(group === "alert" ? Alert : Counter));
+</script>
+
+<div style:margin-bottom="1rem">
+  <label><input type="radio" bind:group value="alert" /> Alert</label>
+  <label><input type="radio" bind:group value="counter" /> Counter</label>
+</div>
+
+<Component />
