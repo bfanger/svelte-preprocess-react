8bc89a8c8472f659225182c2f7ce0e22c84f1b9e
Bob Fanger <bob.fanger@this.nl>
2025-10-15 16:28:04 +0200

    fix: React context (one level)

--- src/lib/internal/SveltifiedSSR.svelte
+++ src/lib/internal/SveltifiedSSR.svelte
@@ -3,7 +3,7 @@
    * Render a React component as a Svelte component.
    */
   import { createElement, use, type Attributes } from "react";
-  import { getAllContexts, type Snippet } from "svelte";
+  import { getAllContexts, getContext, setContext, type Snippet } from "svelte";
   import { renderToReadableStream } from "react-dom/server";
   import ReactContext from "./ReactContext.js";
   import { render } from "svelte/server";
@@ -17,17 +17,76 @@
   const { react$component, react$children, children, ...props }: Props =
     $props();
 
+  const parent = getContext<SveltifiedSSRContext | undefined>("SveltifiedSSR");
   const context = getAllContexts();
+
+  type SveltifiedSSRContext =
+    | {
+        prefix: string;
+        siblingIndex: number;
+        defer: (vdom: ReturnType<typeof createElement>) => void;
+        replace: (id: string) => void;
+      }
+    | undefined;
+
+  const nestedChildren: ReturnType<typeof createElement>[] = [];
+  const replacements: string[] = [];
+
+  let childIndex = 0;
+  const current = setContext<SveltifiedSSRContext>("SveltifiedSSR", {
+    prefix: parent ? `${parent.prefix + parent.siblingIndex}-` : "-",
+    siblingIndex: parent ? parent.siblingIndex++ : 0,
+    defer: (vdom) => {
+      const key = current.prefix + childIndex;
+      current.replace(key);
+      childIndex = nestedChildren.push(
+        createElement(`sveltified-ssr-source${key}`, { key }, vdom),
+      );
+    },
+    replace(id: string) {
+      if (parent) {
+        parent.replace(id);
+      }
+      replacements.push(id);
+    },
+  })!;
 
   const reactChildren = children ? createElement(Child) : react$children;
 
   const vdom = createElement(
     ReactContext,
-    { value: { context, reactChildren, svelteChildren: children } },
+    {
+      value: { context, reactChildren, svelteChildren: children },
+    },
     createElement(react$component, props, reactChildren),
   );
 
-  const html = await streamToString(await renderToReadableStream(vdom));
+  // svelte-ignore non_reactive_update
+  let html = await reactToHtml();
+
+  for (const id of replacements) {
+    const sourceTag = `sveltified-ssr-source${id}`;
+    const sourceStart = html.indexOf(`<${sourceTag}>`);
+    const sourceEnd = html.indexOf(`</${sourceTag}>`);
+    let content = "";
+    if (sourceStart !== -1 && sourceEnd !== -1) {
+      content = html.slice(sourceStart + sourceTag.length + 2, sourceEnd);
+      html =
+        html.slice(0, sourceStart) +
+        html.slice(sourceEnd + sourceTag.length + 3);
+    }
+    const targetTag = `sveltified-ssr-target${id}`;
+    html = html.replaceAll(`<${targetTag}></${targetTag}>`, content);
+  }
+
+  async function reactToHtml() {
+    if (parent) {
+      parent.defer(vdom);
+      const tag = `sveltified-ssr-target${parent.prefix + childIndex}`;
+      return `<${tag}></${tag}>`;
+    }
+    return await streamToString(await renderToReadableStream(vdom));
+  }
 
   async function streamToString(stream: ReadableStream) {
     let output = "";
@@ -49,7 +108,7 @@
         "svelte-preprocess-react: Can't pass react & svelte children at the same time",
       );
     }
-    if (children) {
+    if (children && !parent) {
       const { body, head } = await render(SnippetComponent, {
         props: { snippet: children },
         context: ctx!.context,
@@ -60,9 +119,13 @@
           head,
         );
       }
-      return createElement("sveltify-ssr-child", {
-        dangerouslySetInnerHTML: { __html: body },
-      });
+      return [
+        createElement("sveltify-ssr-child", {
+          key: "svelte-child",
+          dangerouslySetInnerHTML: { __html: body },
+        }),
+        ...nestedChildren,
+      ];
     }
     return undefined;
   }
